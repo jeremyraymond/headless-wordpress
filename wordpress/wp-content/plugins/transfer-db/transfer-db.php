9b39758620b1f9976952b9ee55bd14428ad89c18
jeremyraymond <jeremy.r.raymond@gmail.com>
2015-08-25 02:12:31 -0400

    Basic plugin functions
    Setting up some of the basic functions necessary for a plugin, still
    need to add the settings and the settings form.

--- wordpress/wp-content/plugins/transfer-db/transfer-db.php
+++ wordpress/wp-content/plugins/transfer-db/transfer-db.php
@@ -8,3 +8,22 @@
  * Author URI: http://jeremy-raymond.com
  * License: GPL2
  */
+
+add_action('admin_menu', 'transfer_db_menu');
+
+function transfer_db_menu() {
+	add_submenu_page( 'tools.php', 'Transfer DB', 'Transfer DB', 'administrator', 'transfer-db', 'transfer_db_page' );
+}
+
+function transfer_db_page() {
+	$path = dirname();
+	include $path . 'settings-form.php';
+}
+
+add_action( 'admin_init', 'transfer_db_settings' );
+
+function transfer_db_settings() {
+	register_setting( 'transfer-db-settings-group', 'accountant_name' );
+	register_setting( 'transfer-db-settings-group', 'accountant_phone' );
+	register_setting( 'transfer-db-settings-group', 'accountant_email' );
+}
