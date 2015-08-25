c160e13998fa455f2f1b64a0a3cc5b27c0661e2a
jeremyraymond <jeremy.r.raymond@gmail.com>
2015-08-25 01:35:35 -0400

    Plugin basics setup
    Set up folder for transfer-db plugin and set up the basics.

---
+++ wordpress/wp-content/plugins/transfer-db/transfer-db.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Transfer DB
+ * Plugin URI: http://jeremy-raymond.com
+ * Description: This plugin allows you to quickly and easily transfer DB content from one Wordpress site to another.
+ * Version: 1.0.0
+ * Author: Jeremy Raymond
+ * Author URI: http://jeremy-raymond.com
+ * License: GPL2
+ */
